146fc63cbbe0a2ba82b857ae6bc748b271a95fb0
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-09-28 11:46:49 -0400

    log in form done

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { Button, Form, Header, Grid } from 'semantic-ui-react'
+import { Button, Form, Header, Grid, Container } from 'semantic-ui-react'
 
 class LoginForm extends React.Component {
 
@@ -29,30 +29,35 @@ class LoginForm extends React.Component {
     render() {
         return(
             <>
-                <Grid >
-                    <Grid.Row centered columns={2} >
-                        <Grid.Column >
-                            <Header as="h1" id="loginheader" >Welcome back!</Header>
-                            <Header as="h2" id="loginsubheader">Log in here.</Header>
+                <Container>
+                    <Grid columns={2} id="logingrid">
+                        {/* <Grid.Row  > */}
+                            
+                            <Grid.Column centered textAlign="center" >
+                                <Header as="h1" id="loginheader" >Welcome back!</Header>
+                                <Header as="h2" id="loginsubheader">Log in here  ➯</Header>
 
-                            <p id="loginmessage" >Don't have an account? <u onClick={this.switchToSigninHelper} >Sign up here</u></p>
-                        </Grid.Column>
-                        <Grid.Column >
-                            <Form onSubmit={this.loginHelper}>
-                                <Form.Field>
-                                    <label className="loginlabel">EMAIL</label>
-                                    <input name="user_email" onChange={this.loginChangeHelper} type="text" value={this.state.user_email} className="signinupinput"/><br/><br/>
-                                </Form.Field>
-                                <Form.Field className="signinupinput">
-                                    <label className="loginlabel">PASSWORD</label>
-                                    <input name="password" onChange={this.loginChangeHelper} type="password" value={this.state.password} className="signinupinput"/><br/><br/>
-                                </Form.Field>
-                                <Button type="submit"  className="login">Log In</Button>
-                                {/* Above button originally had this as a prop: width='16' make sure it isn't messing with style to take out */}
-                            </Form>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
+                                <p id="loginmessage" >Don't have an account? <u onClick={this.switchToSigninHelper} >Sign up here</u></p>
+                            </Grid.Column >
+                            
+                            <Grid.Column centered id="loginrightcolumn" >
+                                <Form onSubmit={this.loginHelper}>
+                                    <Form.Field>
+                                        <label className="loginlabel">EMAIL</label>
+                                        <input name="user_email" onChange={this.loginChangeHelper} type="text" value={this.state.user_email} className="signinupinput"/><br/><br/>
+                                    </Form.Field>
+                                    <Form.Field className="signinupinput">
+                                        <label className="loginlabel">PASSWORD</label>
+                                        <input name="password" onChange={this.loginChangeHelper} type="password" value={this.state.password} className="signinupinput"/><br/><br/>
+                                    </Form.Field>
+                                    <Button type="submit"  className="login">Log In</Button>
+                                    {/* Above button originally had this as a prop: width='16' make sure it isn't messing with style to take out */}
+                                </Form>
+                            </Grid.Column >
+                            
+                        {/* </Grid.Row> */}
+                    </Grid>
+                </Container>
             </>
         )
     }
--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { Button, Form, Header } from 'semantic-ui-react'
+import { Button, Form, Header, Grid } from 'semantic-ui-react'
 
 class SignupForm extends React.Component {
 
@@ -33,30 +33,31 @@ class SignupForm extends React.Component {
     render() {
         return(
             <>
-                <Header as="h3"> Nice to meet you! Signup below:</Header>
-                <br/>
-                <Form onSubmit={this.signupHelper}>
-                    <Form.Field>
-                        <label>First Name</label>
-                        <input name="first_name" onChange={this.signupChangeHelper} type="text" value={this.state.first_name} className="signinupinput" />
-                    </Form.Field><br/>
-                    <Form.Field>
-                        <label>Last Name</label>
-                        <input name="last_name" onChange={this.signupChangeHelper} type="text" value={this.state.last_name} className="signinupinput"/>
-                    </Form.Field><br/>
-                    <Form.Field>
-                        <label>Email</label>
-                        <input name="user_email" onChange={this.signupChangeHelper} type="text" value={this.state.user_email} className="signinupinput"/>
-                    </Form.Field><br/>
-                    <Form.Field>
-                        <label>Create Password</label>
-                        <input name="password" onChange={this.signupChangeHelper} type="password" value={this.state.password} className="signinupinput"/>
-                    </Form.Field><br/>
-                    <Button type="submit" className="submit">Sign Up</Button>
-                </Form>
-                <br/>
-                <p>Already have an account? <u onClick={this.switchToLoginHelper} >Log in here</u></p>
+                <Grid centered columns={2}>
+                    <Header as="h3"> Nice to meet you! Signup below:</Header>
+                    <p>Already have an account? <u onClick={this.switchToLoginHelper} >Log in here</u></p>
+
+                    <Form onSubmit={this.signupHelper}>
+                        <Form.Field>
+                            <label>FIRST NAME</label>
+                            <input name="first_name" onChange={this.signupChangeHelper} type="text" value={this.state.first_name} className="signinupinput" />
+                        </Form.Field><br/>
+                        <Form.Field>
+                            <label>LAST NAME</label>
+                            <input name="last_name" onChange={this.signupChangeHelper} type="text" value={this.state.last_name} className="signinupinput"/>
+                        </Form.Field><br/>
+                        <Form.Field>
+                            <label>EMAIL</label>
+                            <input name="user_email" onChange={this.signupChangeHelper} type="text" value={this.state.user_email} className="signinupinput"/>
+                        </Form.Field><br/>
+                        <Form.Field>
+                            <label>CREATE PASSWORD</label>
+                            <input name="password" onChange={this.signupChangeHelper} type="password" value={this.state.password} className="signinupinput"/>
+                        </Form.Field><br/>
+                        <Button type="submit" className="submit">Sign Up</Button>
+                    </Form>
                 
+                </Grid>
             </>
         )
     }
