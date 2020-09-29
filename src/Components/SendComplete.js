26893ac1bad403a767d4b8e50c6074d484b02f1d
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-09-29 13:25:42 -0400

    test run through working

--- src/Components/SendComplete.js
+++ src/Components/SendComplete.js
@@ -13,7 +13,7 @@ class SendComplete extends React.Component{
             <>
                 <h1>Congrats!</h1>      
                 <h2>Letters sent successfully (below image will be thumbs animation)</h2>
-                <img src="https://media.giphy.com/media/l0ExgFoUtST2Q65yw/source.gif"/>
+                <img src="https://media.giphy.com/media/ckB9wvcONerp73fvkQ/source.gif"/>
                 <p>Your executors should get an email with instructions and informatiom about your letters and the other executoprs. (you will be CC's on this so you can be sure they went through)</p>
             
                 <Button onClick={this.backToProfileHelper}>Back to Profile</Button>
