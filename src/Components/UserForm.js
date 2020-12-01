0dd9c4d82357418ab50f9844db96165b1bc731d9
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-12-01 09:24:51 -0500

    userform notes

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Header, Button, Message, Container, Form } from 'semantic-ui-react'
 
+// User info form in Profile
+
 class UserForm extends React.Component {
     
     state = {
@@ -10,10 +12,12 @@ class UserForm extends React.Component {
         password: this.props.currentUser.password
     }
 
+    // Controls form
     userFormChangeHelper = (e) => {
         this.setState({[e.target.name]:e.target.value})
     }
 
+    // Calls function in app - patch request to change user info - then changes form status in profile back to not being edited
     editUserInfoHelper = (e) => {
         e.preventDefault()
         this.props.editUserInfoHandler(this.state)
@@ -24,30 +28,30 @@ class UserForm extends React.Component {
         return(
             <>                
                 <div id="edituserform">
-                <Form onSubmit={this.editUserInfoHelper} >
-                    <Form.Field>
-                        <label className="userlabel">FIRST NAME</label>
-                        <input name="first_name" onChange={this.userFormChangeHelper} value={this.state.first_name} type="text" className="userforminput" />
-                    </Form.Field>
+                    <Form onSubmit={this.editUserInfoHelper} >
+                        <Form.Field>
+                            <label className="userlabel">FIRST NAME</label>
+                            <input name="first_name" onChange={this.userFormChangeHelper} value={this.state.first_name} type="text" className="userforminput" />
+                        </Form.Field>
 
-                    <Form.Field>
-                    <label className="userlabel">LAST NAME</label>
-                    <input name="last_name" onChange={this.userFormChangeHelper} value={this.state.last_name} type="text" className="userforminput" />
-                    </Form.Field>
+                        <Form.Field>
+                            <label className="userlabel">LAST NAME</label>
+                            <input name="last_name" onChange={this.userFormChangeHelper} value={this.state.last_name} type="text" className="userforminput" />
+                        </Form.Field>
 
-                    <Form.Field>
-                    <label className="userlabel">EMAIL</label>
-                    <input name="user_email" onChange={this.userFormChangeHelper} value={this.state.user_email} type="text" className="userforminput" />
-                    </Form.Field>
+                        <Form.Field>
+                            <label className="userlabel">EMAIL</label>
+                            <input name="user_email" onChange={this.userFormChangeHelper} value={this.state.user_email} type="text" className="userforminput" />
+                        </Form.Field>
 
-                    <Form.Field>
-                    <label className="userlabel">PASSWORD</label>
-                    <input name="password" onChange={this.userFormChangeHelper} value={this.state.password} type="text" className="userforminput" />
-                    </Form.Field>
+                        <Form.Field>
+                            <label className="userlabel">PASSWORD</label>
+                            <input name="password" onChange={this.userFormChangeHelper} value={this.state.password} type="text" className="userforminput" />
+                        </Form.Field>
 
-                    <Button onClick={this.props.editStatusChanger} id="canceleditprofilebutton" >Cancel Edit</Button>
-                    <Button type="submit" id="savechangesprofilebutton">Save Changes</Button>
-                </Form>
+                        <Button onClick={this.props.editStatusChanger} id="canceleditprofilebutton" >Cancel Edit</Button>
+                        <Button type="submit" id="savechangesprofilebutton">Save Changes</Button>
+                    </Form>
                 </div>
             </>
         )
